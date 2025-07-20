92abcbb0f544903498396f6426ac8ec04dfcc463
sini <71178237+tlsdl6942@users.noreply.github.com>
2025-07-21 01:45:07 +0900

    Fix: debug router

--- shogi/__init__.py
+++ shogi/__init__.py
@@ -5,3 +5,4 @@ shogi_bp = Blueprint("shogi", __name__, url_prefix="/Shogi")
 from . import routes  # 반드시 import 해줘야 라우트 등록됨
 from shogi.routes import RoomRouter
 from shogi.routes import ShogiRouter
+from shogi.routes import DebugRouter
