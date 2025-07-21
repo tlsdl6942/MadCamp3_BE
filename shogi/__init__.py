c9c2c6d96ce8f526f192e2eb7116e39b7a584c80
sini <71178237+tlsdl6942@users.noreply.github.com>
2025-07-21 17:41:33 +0900

    Fix: 라우터

--- shogi/__init__.py
+++ shogi/__init__.py
@@ -6,3 +6,5 @@ from . import routes  # 반드시 import 해줘야 라우트 등록됨
 from shogi.routes import RoomRouter
 from shogi.routes import ShogiRouter
 from shogi.routes import DebugRouter
+from shogi.routes import TurnRouter
+from shogi.routes import LoginRouter
